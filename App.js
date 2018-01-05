97e6015db279341225933f588a6471af5decefd8
Vinicius <Vinicius@w3get.com.br>
2018-01-05 13:46:08 -0200

    first commit

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // adb shell input keyevent 82
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Modal } from 'react-native';
 import MapVini from './MapVini.js';
 
 
