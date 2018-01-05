109f93591ae1d5a60ebd7ae1f1cf4e1bf5c3868d
Vinicius <Vinicius@w3get.com.br>
2018-01-05 15:22:59 -0200

    first commit

--- App.js
+++ App.js
@@ -1,18 +1,41 @@
 // adb shell input keyevent 82
 import React from 'react';
-import { StyleSheet, Text, View,Modal,Button  } from 'react-native';
+import { StyleSheet, Text, View,Modal,Button,Platform  } from 'react-native';
 import MapVini from './MapVini.js';
-import { MapView } from 'expo';
-
+import { MapView,Constants, Location, Permissions } from 'expo';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {isModalVisible:true};
+    this.state = {isModalVisible:true, 
+                  location: null,
+                  errorMessage: null};
     //BINDAR
     this.fechaModal = this.fechaModal.bind(this)
     this.abrirModal = this.abrirModal.bind(this)
   }
+
+  componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+  };
 
   fechaModal() {
     this.setState({ isModalVisible: false})
@@ -28,10 +51,35 @@ export default class App extends React.Component {
 
   render() {
     //let visivel = String(this.state.isModalVisible);
+    let text = 'Waiting..';
+    if (this.state.errorMessage) {
+      text = this.state.errorMessage;
+    } else if (this.state.location) {
+      text = JSON.stringify(this.state.location);
+    }
+
+   let mapa =  <MapView
+            style={{ flex: 1, height:'40%' }}
+            initialRegion={{
+              latitude: this.state.location.coords.latitude,
+              longitude: this.state.location.coords.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          >
+              <MapView.Marker
+                coordinate= {{latitude: this.state.location.coords.latitude,
+                              longitude: this.state.location.coords.longitude
+                            }}
+                title="Minha localização"
+                description="Estou aqui"
+              />
+          </MapView>;
+
     return (
      
       <View style={styles.container}>
-        
+        <Text> Posicao: {text}</Text>
         <Text>Modal: {this.state.isModalVisible.toString()}</Text>
         
         <Button title="Abrir" onPress={this.abrirModal}  />
@@ -39,15 +87,7 @@ export default class App extends React.Component {
         <Modal visible={this.state.isModalVisible} onRequestClose={this.handleonRequestClose} animationType={'slide'}>
           <Text>Estado: {this.state.isModalVisible.toString()}</Text>
           <Button title="Fechar" onPress={this.fechaModal}  />
-           <MapView
-            style={{ flex: 1, height:'40%' }}
-            initialRegion={{
-              latitude: 37.78825,
-              longitude: -122.4324,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
-            }}
-          />
+          {this.state.location.coords != null ? mapa : ''}
         </Modal>
       
       </View>
